71c2bbb29248e14fc8a34c9f3479bff2b0d7a1bd
Cazpian Levén <cazpians@gmail.com>
2022-05-17 22:13:55 +0200

    Added coordinates alongside gameboard

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -10,14 +10,44 @@ const Game = () => {
 
         <div className="">
             {/* Name of user */}
-            <div className="d-flex flex-column align-items-center">
+            <div className="d-flex flex-column align-items-center" style={{ width: "400px" }}>
                 <h3>Username</h3>
 
                 <button className="mb-5 ready-btn">Ready ?</button>
             </div>
             
+            <div className="d-flex flex-column" style={{ position: "relative", right: "44px" }}>
+                <div className="grid-container justify-content-end" style={{ width: "400px", position: "relative", left: "40px" }}>
+                    <div className="grid-item d-flex justify-content-center align-items-end black-border">1</div>
+                    <div className="grid-item d-flex justify-content-center align-items-end black-border">2</div>
+                    <div className="grid-item d-flex justify-content-center align-items-end black-border">3</div>
+                    <div className="grid-item d-flex justify-content-center align-items-end black-border">4</div>
+                    <div className="grid-item d-flex justify-content-center align-items-end black-border">5</div>
+                    <div className="grid-item d-flex justify-content-center align-items-end black-border">6</div>
+                    <div className="grid-item d-flex justify-content-center align-items-end black-border">7</div>
+                    <div className="grid-item d-flex justify-content-center align-items-end black-border">8</div>
+                    <div className="grid-item d-flex justify-content-center align-items-end black-border">9</div>
+                    <div className="grid-item d-flex justify-content-center align-items-end black-border">10</div>
+                </div>
+                
+                <div className="d-flex">
+                    <div className="grid-container d-flex flex-column" style={{  }}>
+                        <div className="grid-item d-flex justify-content-end align-items-center black-border me-1">A</div>
+                        <div className="grid-item d-flex justify-content-end align-items-center black-border">B</div>
+                        <div className="grid-item d-flex justify-content-end align-items-center black-border">C</div>
+                        <div className="grid-item d-flex justify-content-end align-items-center black-border">D</div>
+                        <div className="grid-item d-flex justify-content-end align-items-center black-border">E</div>
+                        <div className="grid-item d-flex justify-content-end align-items-center black-border">F</div>
+                        <div className="grid-item d-flex justify-content-end align-items-center black-border">G</div>
+                        <div className="grid-item d-flex justify-content-end align-items-center black-border">H</div>
+                        <div className="grid-item d-flex align-items-center black-border align-self-end i">I</div>
+                        <div className="grid-item d-flex justify-content-end align-items-center black-border">J</div>
+                    </div>
 
-            <Gameboard />
+                    <Gameboard />
+                </div>
+            </div>
+           
 
             {/* Your ships, place them out on the board */}
             <div>
