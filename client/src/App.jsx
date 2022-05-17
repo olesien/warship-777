4be61f144bbded772c4fd7069e08ae596181b9c7
Cazpian Levén <cazpians@gmail.com>
2022-05-17 10:42:08 +0200

    Fixed rotate button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,10 @@
 import Startpage from "./pages/Startpage";
 import Game from "./pages/Game";
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
     return (
         <div className="container text-center">
-            <FontAwesomeIcon icon={faCoffee} />
             {/* Name submits, add className "d-none" to first div */}
 
             <div className="d-none">
