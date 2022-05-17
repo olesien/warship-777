a93e5cfcfcfa2715d4e117d9a941fd03ffd3a4a7
olesien <olesien2@gmail.com>
2022-05-17 10:29:21 +0200

    Added font awesome

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,26 @@
-import Startpage from './pages/Startpage';
-import Game from './pages/Game';
-import './App.css';
+import Startpage from "./pages/Startpage";
+import Game from "./pages/Game";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
-	return (
-		<div className="container text-center">
+    return (
+        <div className="container text-center">
+            <FontAwesomeIcon icon={faCoffee} />
+            {/* Name submits, add className "d-none" to first div */}
 
-			{/* Name submits, add className "d-none" to first div */}
+            <div className="d-none">
+                <Startpage />
+            </div>
 
-			<div className="d-none">
-			<Startpage />
+            {/* Remove className "d-none" from second div */}
 
-			</div>
-
-			{/* Remove className "d-none" from second div */}
-
-			<div className="">
-			<Game />
-
-			</div>
-			
-		</div>
-	)
-}
+            <div className="">
+                <Game />
+            </div>
+        </div>
+    );
+};
 
 export default App;
