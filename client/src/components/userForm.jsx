393b6b2f35e25979ac1fa59e279c8ed33c36f6bb
Tsubarah <Tsubarah@hotmail.se>
2022-05-19 16:33:45 +0200

    Disabled button when clicked and changed styling. Added cursor

--- client/src/components/userForm.jsx
+++ client/src/components/userForm.jsx
@@ -6,16 +6,21 @@ const UserForm = ({ username, setUsername, onSubmit, loading}) => {
     <div>
       <form onSubmit={onSubmit} className="d-flex flex-column align-items-center">
         <input 
-          disabled={loading}
           type="text" 
           id="userName" 
           name="userName" 
           value={username}
           placeholder="Enter name..." 
-          onChange={e => setUsername(e.target.value)} />
+          onChange={e => setUsername(e.target.value)} 
+        />
           {/* <UserInput /> */}
         {/* <br/> */}
-        <input type="submit" value="Fight!" id="fightBtn"/>
+        <input 
+          type="submit" 
+          value="Fight!" 
+          id="fightBtn"
+          disabled={loading}
+        />
       </form>
     </div>
   )
