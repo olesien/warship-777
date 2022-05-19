7857c81cb79b98fad51047bd9e40d58bc419b332
Cazpian Levén <cazpians@gmail.com>
2022-05-19 13:41:45 +0200

    Added avatar select with name/changed margin-bottom

--- client/src/pages/Startpage.jsx
+++ client/src/pages/Startpage.jsx
@@ -6,6 +6,19 @@ const Startpage = ({ onSubmit }) => {
   const [username, setUsername] = useState('')
   const { socket, setChatUsername, chatUsername } = useChatContext()
 
+  const one = "Monkey D. Luffy"
+  const two = "Roronoa Zoro"
+  const three = "Shanks"
+  const four = "Nami"
+  const five = "Dracule Mihawk"
+  const six = "Karasu"
+  const seven = "Nefertari Vivi"
+  const eight = "Arlong"
+
+  const avatarName = (name) => {
+    setUsername(name)
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
 
@@ -44,16 +57,47 @@ const Startpage = ({ onSubmit }) => {
                 />
             </div>
 
-            <div id="avatarSelect">
-                <div className="avatar" id="one"></div>
-                <div className="avatar" id="two"></div>
-                <div className="avatar" id="three"></div>
-                <div className="avatar" id="four"></div>
-                <div className="avatar" id="five"></div>
-                <div className="avatar" id="six"></div>
-                <div className="avatar" id="seven"></div>
-                <div className="avatar" id="eight"></div>
-            </div>
+              <form action="" id="avatarSelect" >
+                <label className="avatar" id="one">
+                  <input type="radio" name="avatar" onClick={() => avatarName(one)}/>
+                  <label htmlFor="" className="avatarLabel" />
+                </label>
+
+                <label className="avatar" id="two">
+                  <input type="radio" name="avatar" onClick={() => avatarName(two)}/>
+                  <label htmlFor="" className="avatarLabel" />
+                </label>
+
+                <label className="avatar" id="three">
+                  <input type="radio" name="avatar" onClick={() => avatarName(three)}/>
+                  <label htmlFor="" className="avatarLabel" />
+                </label>
+
+                <label className="avatar" id="four">
+                  <input type="radio" name="avatar" onClick={() => avatarName(four)}/>
+                  <label htmlFor="" className="avatarLabel" />
+                </label>
+
+                <label className="avatar" id="five">
+                  <input type="radio" name="avatar" onClick={() => avatarName(five)}/>
+                  <label htmlFor="" className="avatarLabel" />
+                </label>
+
+                <label className="avatar" id="six">
+                  <input type="radio" name="avatar" onClick={() => avatarName(six)}/>
+                  <label htmlFor="" className="avatarLabel" />
+                </label>
+
+                <label className="avatar" id="seven">
+                  <input type="radio" name="avatar" onClick={() => avatarName(seven)}/>
+                  <label htmlFor="" className="avatarLabel" />
+                </label>
+
+                <label className="avatar" id="eight">
+                  <input type="radio" name="avatar" onClick={() => avatarName(eight)}/>
+                  <label htmlFor="" className="avatarLabel" />
+                </label>
+              </form>
 
             <div id="waitingText" className="d-none">
                 <h1>Waiting for player...</h1>
