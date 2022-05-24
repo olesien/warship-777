049d5e0409849f7134dd429d4f844f1db5cccba1
Cazpian Levén <cazpians@gmail.com>
2022-05-24 11:59:10 +0200

    Added enemy grid

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -93,85 +93,174 @@ const Game = () => {
                     </button>
                 </div>
 
-                <div className="d-flex flex-column" id="playFieldPosition">
-                    <div
-                        className="grid-container justify-content-end w-400"
-                        id="nmrPosition"
-                    >
-                        <div className="grid-item d-flex justify-content-center align-items-end black-border">
-                            1
-                        </div>
-                        <div className="grid-item d-flex justify-content-center align-items-end black-border">
-                            2
-                        </div>
-                        <div className="grid-item d-flex justify-content-center align-items-end black-border">
-                            3
-                        </div>
-                        <div className="grid-item d-flex justify-content-center align-items-end black-border">
-                            4
-                        </div>
-                        <div className="grid-item d-flex justify-content-center align-items-end black-border">
-                            5
-                        </div>
-                        <div className="grid-item d-flex justify-content-center align-items-end black-border">
-                            6
-                        </div>
-                        <div className="grid-item d-flex justify-content-center align-items-end black-border">
-                            7
-                        </div>
-                        <div className="grid-item d-flex justify-content-center align-items-end black-border">
-                            8
-                        </div>
-                        <div className="grid-item d-flex justify-content-center align-items-end black-border">
-                            9
-                        </div>
-                        <div className="grid-item d-flex justify-content-center align-items-end black-border">
-                            10
-                        </div>
-                    </div>
+                <div className="d-flex align-items-center">
+                    {player.ready 
+                        ? (opponent.ready 
+                            ? <div className="d-flex flex-column" id="playFieldPosition">
+                            <div
+                                className="grid-container justify-content-end w-400"
+                                id="nmrPosition"
+                            >
+                                <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                    1
+                                </div>
+                                <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                    2
+                                </div>
+                                <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                    3
+                                </div>
+                                <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                    4
+                                </div>
+                                <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                    5
+                                </div>
+                                <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                    6
+                                </div>
+                                <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                    7
+                                </div>
+                                <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                    8
+                                </div>
+                                <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                    9
+                                </div>
+                                <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                    10
+                                </div>
+                            </div>
+    
+                            <div className="d-flex">
+                                <div className="grid-container d-flex flex-column">
+                                    <div className="grid-item d-flex justify-content-end align-items-center black-border me-1">
+                                        A
+                                    </div>
+                                    <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                        B
+                                    </div>
+                                    <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                        C
+                                    </div>
+                                    <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                        D
+                                    </div>
+                                    <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                        E
+                                    </div>
+                                    <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                        F
+                                    </div>
+                                    <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                        G
+                                    </div>
+                                    <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                        H
+                                    </div>
+                                    <div className="grid-item d-flex align-items-center black-border align-self-end i">
+                                        I
+                                    </div>
+                                    <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                        J
+                                    </div>
+                                </div>
+    
+                                <Gameboard
+                                    grid={grid}
+                                    drop={drop}
+                                    allowDrop={allowDrop}
+                                    drag={drag}
+                                />
+                            </div>
+                        </div> 
+                            : "") 
+                        : ""}
+                
 
-                    <div className="d-flex">
-                        <div className="grid-container d-flex flex-column">
-                            <div className="grid-item d-flex justify-content-end align-items-center black-border me-1">
-                                A
+                    <div className="d-flex flex-column" id="playFieldPosition">
+                        <div
+                            className="grid-container justify-content-end w-400"
+                            id="nmrPosition"
+                        >
+                            <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                1
                             </div>
-                            <div className="grid-item d-flex justify-content-end align-items-center black-border">
-                                B
+                            <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                2
                             </div>
-                            <div className="grid-item d-flex justify-content-end align-items-center black-border">
-                                C
+                            <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                3
                             </div>
-                            <div className="grid-item d-flex justify-content-end align-items-center black-border">
-                                D
+                            <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                4
                             </div>
-                            <div className="grid-item d-flex justify-content-end align-items-center black-border">
-                                E
+                            <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                5
                             </div>
-                            <div className="grid-item d-flex justify-content-end align-items-center black-border">
-                                F
+                            <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                6
                             </div>
-                            <div className="grid-item d-flex justify-content-end align-items-center black-border">
-                                G
+                            <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                7
                             </div>
-                            <div className="grid-item d-flex justify-content-end align-items-center black-border">
-                                H
+                            <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                8
                             </div>
-                            <div className="grid-item d-flex align-items-center black-border align-self-end i">
-                                I
+                            <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                9
                             </div>
-                            <div className="grid-item d-flex justify-content-end align-items-center black-border">
-                                J
+                            <div className="grid-item d-flex justify-content-center align-items-end black-border">
+                                10
                             </div>
                         </div>
 
-                        <Gameboard
-                            grid={grid}
-                            drop={drop}
-                            allowDrop={allowDrop}
-                            drag={drag}
-                        />
+                        <div className="d-flex">
+                            <div className="grid-container d-flex flex-column">
+                                <div className="grid-item d-flex justify-content-end align-items-center black-border me-1">
+                                    A
+                                </div>
+                                <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                    B
+                                </div>
+                                <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                    C
+                                </div>
+                                <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                    D
+                                </div>
+                                <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                    E
+                                </div>
+                                <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                    F
+                                </div>
+                                <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                    G
+                                </div>
+                                <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                    H
+                                </div>
+                                <div className="grid-item d-flex align-items-center black-border align-self-end i">
+                                    I
+                                </div>
+                                <div className="grid-item d-flex justify-content-end align-items-center black-border">
+                                    J
+                                </div>
+                            </div>
+
+                            <Gameboard
+                                grid={grid}
+                                drop={drop}
+                                allowDrop={allowDrop}
+                                drag={drag}
+                            />
+                        </div>
                     </div>
                 </div>
+
+                
 
                 {/* Your ships, place them out on the board */}
                 <div>
