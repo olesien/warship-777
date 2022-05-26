18f415add939460bbe37e4b44c21759e910bfc3a
olesien <olesien2@gmail.com>
2022-05-26 21:30:25 +0200

    Added full boat preview
    + next make it look pretty

--- client/src/components/PreviewShips.jsx
+++ client/src/components/PreviewShips.jsx
@@ -1,36 +1,46 @@
 import { useEffect, useState } from "react";
 import { useGameContext } from "../contexts/GameContextProvider";
 
+import RenderBoat from "./RenderBoat";
+
+const makeShips = (person) => {
+    const gameboard = person.gameboard;
+    let coreParts = [];
+    gameboard.forEach((col, colIndex) => {
+        col.forEach((row, rowIndex) => {
+            if (row.filled) {
+                const newRow = row;
+                newRow.subparts = row.subparts.map((part) => {
+                    const partColIndex = colIndex + part.col;
+                    const partRowIndex = rowIndex + part.row;
+
+                    console.log(partColIndex, partRowIndex);
+                    const child = gameboard[partColIndex][partRowIndex];
+                    return { ...part, hit: child.hit };
+                });
+                coreParts.push(row);
+            }
+        });
+    });
+    console.log(coreParts);
+
+    return coreParts.map((ship, key) => {
+        return <RenderBoat ship={ship} key={key} />;
+    });
+};
+
 export default function PreviewShips({ foe }) {
     const { player, opponent } = useGameContext();
     const [playerShips, setPlayerShips] = useState([]);
     const [opponentShips, setOpponentShips] = useState([]);
     useEffect(() => {
-        console.log(player);
-        console.log(opponent);
-
-        //player
-        const gameboard = player.gameboard;
-        let coreParts = [];
-        gameboard.forEach((col, colIndex) => {
-            col.forEach((row, rowIndex) => {
-                if (row.filled) {
-                    const newRow = row;
-                    newRow.subparts = row.subparts.map((part) => {
-                        const partColIndex = colIndex + part.col;
-                        const partRowIndex = rowIndex + part.row;
-
-                        const child = gameboard[partColIndex][partRowIndex];
-                        return { ...part, hit: child.hit };
-                    });
-                    coreParts.push(row);
-                }
-            });
-        });
-        console.log(coreParts);
-    }, [player, opponent, playerShips, opponentShips]);
+        if (foe) {
+            return setOpponentShips(makeShips(opponent));
+        }
+        return setPlayerShips(makeShips(player));
+    }, [player, opponent, playerShips, opponentShips, foe]);
     if (foe) {
-        return <div>Preview Enemy Ships</div>;
+        return <div>{opponentShips}</div>;
     }
-    return <div>Preview Your Ships</div>;
+    return <div>{playerShips}</div>;
 }
