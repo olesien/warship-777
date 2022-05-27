38f0a368999c777f5190330eb6a04602cb5c0662
Tsubarah <Tsubarah@hotmail.se>
2022-05-27 12:43:30 +0200

    Now loading is working

--- client/src/components/EndGame.jsx
+++ client/src/components/EndGame.jsx
@@ -1,50 +1,71 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import WaitingPlayer from '../components/WaitingPlayer';
 
 export default function EndGame({ socket, winner, room, grid }) {
+  const [loading, setLoading] = useState(false)
+  const [awaitPlayers, setAwaitPlayers] = useState(0)
   const navigate = useNavigate()
 
   const handlePlayAgain = () => {
+    setLoading(true)
+    setAwaitPlayers(prevValue => prevValue + 1)
+    console.log(awaitPlayers)
+
+    if (awaitPlayers === 2) {
     navigate("/")
     socket.emit("game:replay", room, grid)
+    }
   }
+  useEffect(() => {
+    
+  }, [awaitPlayers])
 
   return (
     <div className="endgame-wrapper">
-      <div className="endgame-background-image">
-        <button 
-          className="back-startpage"
-          onClick={() => navigate("/" - "game")}
-        >
-          <FontAwesomeIcon icon={faCircleArrowLeft} className="back-icon"/>
-          Startpage
-        </button>
-        <div className="winner-frame">
-          <div 
-            className={winner.id !== socket.id 
-              ? "transparent-background-red" 
-              : "transparent-background-green"}
+      {/* {!loading &&  */}
+        <div className="endgame-background-image">
+          <button 
+            className="back-startpage"
+            onClick={() => navigate("/" - "game")}
           >
-            {winner.id !== socket.id
-              ? <h3>{winner.username}</h3>
-              : <h3 className="mt-4"> </h3>}
+            <FontAwesomeIcon icon={faCircleArrowLeft} className="back-icon"/>
+            Startpage
+          </button>
+          {loading && 
+            <div className="replay-loading">
+              <WaitingPlayer />
+            </div>
+          }
+          {!loading && 
+          <div className="winner-frame">
+            <div 
+              className={winner.id !== socket.id 
+                ? "transparent-background-red" 
+                : "transparent-background-green"}
+            >
+              {winner.id !== socket.id
+                ? <h3>{winner.username}</h3>
+                : <h3 className="mt-4"> </h3>}
+              
+              <img className="winner-image mt-2" src={winner.avatar} alt="" />
             
-            <img className="winner-image mt-2" src={winner.avatar} alt="" />
-          
-            {winner.id !== socket.id
-              ? <h3>Sunk your ships!</h3>
-              : <h3>You sunk the enemy ships!</h3>}
+              {winner.id !== socket.id
+                ? <h3>Sunk your ships!</h3>
+                : <h3>You sunk the enemy ships!</h3>}
+            </div>
           </div>
+          }
+          <button 
+            className="play-again-btn"
+            onClick={handlePlayAgain}
+          >
+            Play again!
+          </button>
         </div>
-
-        <button 
-          className="play-again-btn"
-          onClick={handlePlayAgain}
-        >
-          Play again!
-        </button>
-      </div>
+      {/* } */}
     </div>
   )
 }
