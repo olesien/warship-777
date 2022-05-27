4f4cf4acc073eb5afe530c826dc434f30a779445
olesien <olesien2@gmail.com>
2022-05-27 12:32:23 +0200

    Fixed some layout issues + placement of red ones
    Now, the "sunk" parts of the ship will always be first!

--- client/src/components/RenderBoat.jsx
+++ client/src/components/RenderBoat.jsx
@@ -7,8 +7,15 @@ export default function RenderBoat({ ship }) {
     const partLength = ship.subparts.length;
     //ship.subparts.unshift({hit: ship.hit})
     useEffect(() => {
-        const newShip = JSON.parse(JSON.stringify(ship));
+        let newShip = JSON.parse(JSON.stringify(ship));
         newShip.subparts.unshift({ hit: ship.hit });
+        newShip.subparts = newShip.subparts.sort((a, b) =>
+            a.hit === b.hit ? 0 : a.hit ? -1 : 1
+        );
+
+        // console.log(
+        //     newShip.subparts.sort((a, b) => (a.hit === b ? 0 : a ? -1 : 1))
+        // );
         setParts(
             newShip.subparts.map((part, key) => (
                 <div
