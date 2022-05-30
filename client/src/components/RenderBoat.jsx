2847d51d02e49f5a621d234d231cc3bbbe0afd6f
olesien <olesien2@gmail.com>
2022-05-30 10:21:56 +0200

    Fixed to only show if boat is sunk/not
    .. instead of showing it for each part

--- client/src/components/RenderBoat.jsx
+++ client/src/components/RenderBoat.jsx
@@ -3,29 +3,29 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 export default function RenderBoat({ ship }) {
-    const [parts, setParts] = useState([]);
+    const [sunk, setSunk] = useState(false);
     const partLength = ship.subparts.length;
     //ship.subparts.unshift({hit: ship.hit})
     useEffect(() => {
         let newShip = JSON.parse(JSON.stringify(ship));
         newShip.subparts.unshift({ hit: ship.hit });
-        newShip.subparts = newShip.subparts.sort((a, b) =>
-            a.hit === b.hit ? 0 : a.hit ? -1 : 1
-        );
-
-        // console.log(
-        //     newShip.subparts.sort((a, b) => (a.hit === b ? 0 : a ? -1 : 1))
+        // newShip.subparts = newShip.subparts.sort((a, b) =>
+        //     a.hit === b.hit ? 0 : a.hit ? -1 : 1
         // );
-        setParts(
-            newShip.subparts.map((part, key) => (
-                <div
-                    key={key}
-                    className={"subpart " + (part.hit ? " hit" : "")}
-                ></div>
-            ))
-        );
+
+        //Has any part NOT been struck?
+        const isSunk = newShip.subparts.find((part) => !part.hit);
+        if (!isSunk) {
+            setSunk(false);
+        } else {
+            setSunk(true);
+        }
     }, [ship]);
-    return <div className={"previewboat boatLength" + partLength}>{parts}</div>;
+    if (sunk) {
+        return <div className={"previewboat boatLength" + partLength}></div>;
+    }
+
+    return <div className={"previewboat hit boatLength" + partLength}></div>;
 
     //return <div className={"previewboat boatLength" + partLength}></div>;
 }
