c95d433bff4261fe72baee7b43b7b045e1eb7682
Benjamin <67978743+Tsubarah@users.noreply.github.com>
2022-06-02 11:16:26 +0200

    play again buttons disapears if it has been pressed and is waiting for the opponent

--- client/src/components/EndGame.jsx
+++ client/src/components/EndGame.jsx
@@ -71,14 +71,14 @@ export default function EndGame({ socket, winner, room, grid, init }) {
                 ? <h3>Sunk your ships!</h3>
                 : <h3>You sunk the enemy ships!</h3>}
             </div>
+            <button 
+              className="play-again-btn"
+              onClick={handlePlayAgain}
+            >
+              Play again!
+            </button>
           </div>
           }
-          <button 
-            className="play-again-btn"
-            onClick={handlePlayAgain}
-          >
-            Play again!
-          </button>
         </div>
     </div>
   )
